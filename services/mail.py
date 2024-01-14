de9d0dc0f3565d57b18fba66d3036dac175a9a6c
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-01-14 00:02:08 -0500

    Add SendGrid mail service

---
+++ services/mail.py
@@ -0,0 +1,19 @@
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+from config import MailConfig
+
+class MailService:
+    
+    def send(dest_email: str, subject: str, html_content: str):
+        message = Mail(
+            from_email=MailConfig.SENDER_EMAIL,
+            to_emails=dest_email,
+            subject=subject,
+            html_content=html_content
+        )
+        sg = SendGridAPIClient(MailConfig.SENDGRID_API_KEY)
+        response = sg.send(message)
+        # print(response.status_code)
+        # print(response.body)
+        # print(response.headers)
