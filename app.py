e863540182ce49c39ac1556abb274a41273a3fe3
Jorge Andres Padilla <jorgeandres.padillasalgado@gmail.com>
2024-01-14 03:12:30 -0500

    Add main app script

---
+++ app.py
@@ -0,0 +1,102 @@
+from flask import Flask
+from services.mail import MailService
+from services.odoo import OdooService
+from services.storage import NextCloudService
+from utils import create_user_email, generate_password
+
+app = Flask(__name__)
+
+mail_service = MailService()
+odoo_service = OdooService()
+nextcloud_service = NextCloudService()
+
+def add(name: str, work_phone: str, private_email: str):
+    email = create_user_email(name, 'example.com')
+
+    # Odoo: create user and employee
+    try:
+        employee_id = odoo_service.create_employee({
+            'name': name,
+            'work_phone': work_phone,
+            'work_email': email,
+            'private_email': private_email,
+        })
+        print(f'Empleado creado con ID {employee_id}')
+
+        user_id = odoo_service.create_user({
+            'name': name,
+            'login': email,
+            'email': email,
+            'employee_ids': [employee_id],
+        })
+        print(f'Usuario creado con ID {user_id}')
+
+    except Exception as e:
+        print(f'Error al crear el empleado: {e}')
+
+    # Nextcloud (storage): create user
+    try:
+        password = generate_password()
+        nextcloud_service.create_user({
+            'email': email,
+            'password': password,
+            'name': name,
+            'quota_in_gb': 5,
+        })
+        print(f'Usuario creado en Nextcloud')
+    except Exception as e:
+        print(f'Error al crear el usuario de Nextcloud: {e}')
+
+    # Send email
+    try:
+        mail_service.send(
+            private_email,
+            'Bienvenido a nuestra plataforma',
+            'Hola,\n\n¡Bienvenido a nuestra plataforma! Gracias por unirte.'
+        )
+        print(f"Se ha enviado un correo a '{private_email}' con las instrucciones")
+    except Exception as e:
+        print(f'Error al enviar el correo: {e}')
+
+
+def delete(email: str):
+    # Odoo: delete user and employee
+    try:
+        user = odoo_service.find_user_by_email(email)
+        if not user:
+            print('No se encontró el usuario')
+        else:
+            odoo_service.delete_user(user[0]['id'])
+            print('Usuario eliminado')
+
+        employee = odoo_service.find_employee_by_email(email)
+        if not employee:
+            print('No se encontró el empleado')
+        else:
+            employee_private_email = employee[0]['private_email']
+            print(employee_private_email) # TODO: delete
+            odoo_service.delete_employee(employee[0]['id'])
+            print('Empleado eliminado')
+    except Exception as e:
+        print(f'Error al eliminar el empleado: {e}')
+
+    # Nextcloud (storage): delete user
+    try:
+        nextcloud_service.delete_user(email)
+        print('Usuario eliminado de Nextcloud')
+    except Exception as e:
+        print(f'Error al eliminar el usuario de Nextcloud: {e}')
+
+    # Send email
+    try:
+        mail_service.send(
+            employee_private_email,
+            'Tu cuenta ha sido eliminada',
+            'Hola,\n\nTu cuenta ha sido eliminada.'
+        )
+        print(f"Se ha enviado un correo a '{employee_private_email}' con el aviso")
+    except Exception as e:
+        print(f'Error al enviar el correo: {e}')
+
+# add('Juan Perez', '1234567890', 'user@gmail.com')
+# delete('juan.perez@example.com')
